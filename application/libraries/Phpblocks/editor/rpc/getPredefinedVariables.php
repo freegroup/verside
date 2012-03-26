c67e46a7787592879c4677644f2c47a4187984d2
Andreas Herz <a.herz@freegroup.de>
2012-03-26 09:53:21 +0200

    remove old files

--- application/libraries/Phpblocks/editor/rpc/getPredefinedVariables.php
+++
@@ -1,4 +0,0 @@
-[
-{"name":"dummy"},
-{"name":"vorname"}
-]
--- js/core_growl.js
+++
@@ -1,9 +0,0 @@
-
-function growl(message, title){
-  $.jGrowl(message, { 
-	beforeOpen:function(e,m,o){$(e).removeClass("ui-corner-all ui-state-highlight");},
-	life:1500,
-	close:false,
-	header: title
- });
-};
